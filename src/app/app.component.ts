03137f835ce46a1a6d0263aabe58d0cfe535b112
Marek Matczak <mmatczak@autograf.pl>
2021-01-21 21:18:58 +0100

    TSLint replaced with ESLint and Prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ba-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'book-app';
